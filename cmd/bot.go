15cb69d961aa390d03088b89e8340d498b91a587
azpect3120 <hhargreaves2006@gmail.com>
2024-11-18 23:56:52 -0700

    FEAT: Data files created/added

--- cmd/bot.go
+++ cmd/bot.go
@@ -1,5 +1,32 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"os"
+	"strings"
+)
 
+func main() {
+	tickers := get_tickers()
+
+	for _, ticker := range tickers {
+		println(ticker)
+	}
+}
+
+func get_tickers() []string {
+	f, err := os.Open("./data/nasdaq_names.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	var tickers []string
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		tickers = append(tickers, strings.TrimSpace(scanner.Text()))
+	}
+
+	return tickers
 }
