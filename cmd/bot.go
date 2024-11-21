1d939aa1c3c092e4ce6d0109f3a0f91bfa172dd8
azpect3120 <hhargreaves2006@gmail.com>
2024-11-21 16:14:18 -0700

    FEAT: Some basic CLI functionality
    Using flags, this is temporary.

--- cmd/bot.go
+++ cmd/bot.go
@@ -1,36 +1,63 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/Azpect3120/TradingBot/api"
 	"github.com/Azpect3120/TradingBot/internal/util"
 )
 
 func main() {
-	rows, err := util.GetNamesFromCSV("./data/large_mega_cap.csv")
-	if err != nil {
-		panic(err)
-	}
+	// Create input flags for CLI
+	symPath := flag.String("f", "", "Path to the CSV file containing symbols")
+	symbol := flag.String("s", "", "Symbol to get data for. Do not use with -f")
+	lookback := flag.Int("l", 500, "Number of days to look back")
+	flag.Parse()
 
-	for _, row := range rows {
-		bars, err := util.GetBars(row, 500)
+	// Symbol was provided
+	if *symbol != "" {
+		bars, err := util.GetBars(*symbol, *lookback)
 		if err != nil {
-			// Failed on these names: need to find a fix
-			// BRK/A: changed to BRK-A
-			// BRK/B: changed to BRK-B
-			println(err.Error(), row)
-			continue
+			println(err.Error(), *symbol)
+			os.Exit(1)
 		}
-		if len(bars) == 0 {
-			println("No bars found for ", row)
-			continue
+		if len(bars) < 50 {
+			println("Not enough bars were found", *symbol)
+			os.Exit(1)
 		}
 		bars = bars[:len(bars)-1]
 
-		rating := api.CalculateRating(row, bars)
-		if rating.LongScore >= 50 || rating.ShortScore >= 50 {
+		rating := api.CalculateRating(*symbol, bars)
+		fmt.Printf("Rating %s Long: %0.2f Short: %0.2f\n", *symbol, rating.LongScore, rating.ShortScore)
+
+	}
+
+	// Path was provided and no symbol
+	if *symPath != "" && *symbol == "" {
+		rows, err := util.GetNamesFromCSV(*symPath)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, row := range rows {
+			bars, err := util.GetBars(row, *lookback)
+			if err != nil {
+				println(err.Error(), row)
+				continue
+			}
+			if len(bars) < 50 {
+				println("Not enough bars were found", row)
+				continue
+			}
+			bars = bars[:len(bars)-1]
+
+			rating := api.CalculateRating(row, bars)
+			// if rating.LongScore >= 50 || rating.ShortScore >= 50 {
 			fmt.Printf("Rating %s Long: %0.2f Short: %0.2f\n", row, rating.LongScore, rating.ShortScore)
+			// }
 		}
 	}
+
 }
